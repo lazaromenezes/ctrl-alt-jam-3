f98e41c27ef886eacf0b8645969515213dfa2167
Lazaro Menezes <lazarodm@gmail.com>
2024-05-10 13:10:20 -0300

    Convert project to 4.2.2

--- dr_boro/scenes/player/player.gd
+++ dr_boro/scenes/player/player.gd
@@ -7,12 +7,8 @@ func _ready():
 	speed = default_speed
 
 func _physics_process(_delta: float) -> void:
-	
 	look_at(get_global_mouse_position())
-	
 	var movement = Input.get_axis("move_backwards", "move_forward")
-
 	velocity = transform.x * speed * movement
-	
 	move_and_slide()
 
